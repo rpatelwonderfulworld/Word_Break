750f832f479a80d7f5b1fbd63aa7069eb3f967df
rpatelwonderfulworld <patelroshnib@gmail.com>
2014-07-23 22:15:22 -0700

    test

---
+++ Word_Break/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word_Break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            wordBreak("AmazonKindle");
+            Console.ReadLine();
+        }
+
+        public static void wordBreak(string strInput)
+        {
+            if (strInput == null)
+                Console.WriteLine("error");
+
+            if (strInput.Length == 0)
+                Console.WriteLine("error");
+
+            char[] arr = strInput.ToCharArray();
+ 
+            StringBuilder sb = new StringBuilder("");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sb.Append(arr[i]);
+
+                if (wordDict(sb) == true )
+                {
+                    sb.Append(" ");
+                    Console.WriteLine(sb);
+                }
+            }
+
+        }
+        public static Boolean wordDict(StringBuilder sb)
+        {
+            string[] strArry = {"Amazon","Kindle","Roshni","Patel"};
+
+            string[] inputStr = sb.ToString().Split(' ');
+
+            foreach (string str in inputStr)
+            {
+                return strArry.Contains(sb.ToString()); 
+            }
+
+            return false;
+    
+        }
+    }
+}
